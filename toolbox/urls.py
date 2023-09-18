ffd8083528672beb565066ad25ac2dbac8b1678a
JarvisXyy <jarvis_xyy@126.com>
2023-09-18 20:36:38 +0800

    整合IP判断和DNS解析到一个函数中

--- toolbox/urls.py
+++ toolbox/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ip_address/', views.ip_address, name='ip_address'),
-    path('dns_lookup/', views.dns_lookup, name='dns_lookup'),
+    path('ip_tools/', views.ip_tools, name='ip_tools')
 ]
