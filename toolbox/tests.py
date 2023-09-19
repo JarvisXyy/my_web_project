287c06ae8afa58b17ac20adfdb2581b3497cc391
JarvisXyy <jarvis_xyy@126.com>
2023-09-19 20:12:20 +0800

    添加api接口测试进行对比

--- toolbox/tests.py
+++ toolbox/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+#API测试
+import requests
 
-# Create your tests here.
+URL = "http://10.23.140.196/NetTrack/GetIpInfo/"
+TOKEN = ""
+headers = {
+    "token": "Bearer " + TOKEN
+}
+with open('ip_test.txt', 'r') as file:
+    ip_addresses = file.readlines()
+for ip in ip_addresses:
+    ip = ip.strip()
+    params = {
+        'ip': ip
+    }
+    response = requests.get(URL, headers=headers, params=params)
+    if response.status_code == 200:
+        print(response.json())
+        print(ip)
+    else:
+        print(f"Error {response.status_code}: {response.text}")
